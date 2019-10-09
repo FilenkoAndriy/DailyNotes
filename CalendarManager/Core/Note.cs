0d62770553239729a69605dd340eb964e5a383bf
ValeriiaAleksieienko <lera.alal@gmail.com>
2019-10-09 19:40:15 +0300

    Added CalendarManager project.

---
+++ CalendarManager/Core/Note.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarManager.Core
+{
+    [Serializable]
+    class Note : INotifyPropertyChanged
+    {
+        DateTime date;
+        string title;
+        string description;
+
+        public int Id { get; set; }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ CalendarManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,105 @@
+﻿using CalendarManager.Core;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CalendarManager.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        string filepath = "notes.json";
+        private DateTime selectedDate = DateTime.Today;
+        private Note selectedNote;
+        public ObservableCollection<Note> notes = new ObservableCollection<Note>();
+
+        public ObservableCollection<Note> Notes
+        {
+            get { return notes; }
+            set
+            {
+                notes = value;
+                OnPropertyChanged("Notes");
+            }
+        }
+
+        public DateTime SelectedDate
+        {
+            get { return selectedDate; }
+            set
+            {
+                selectedDate = value;
+                Notes = GetNotes(selectedDate);
+                OnPropertyChanged("SelectedDate");
+            }
+        }
+
+        public ObservableCollection<Note> GetNotes(DateTime date)
+        {
+            List<Note> items = readJson();
+            ObservableCollection<Note> notes = new ObservableCollection<Note>();
+            foreach (Note note in items)
+            {
+                if(note.Date == date)
+                {
+                    notes.Add(note);
+                }
+            }
+            return notes;
+        }
+
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Note note = new Note { Id = 5, Date = selectedDate, Title = "В универ", Description = "Сдать заявление, подойти к Пономарьову." };
+                      writeJson(note);
+                      notes.Add(note);
+                  }));
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                    (removeCommand = new RelayCommand(obj =>
+                    {
+
+                    }
+                    )
+            }
+        }
+
+        public List<Note> readJson()
+        {
+            List<Note> items = new List<Note>();
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                string jsonRead = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<Note>>(jsonRead);
+            }
+            return items;
+        }
+
+        public void writeJson(Note note)
+        {
+            List<Note> notes = readJson();
+            notes.Add(note);
+            string jsonWrite = JsonConvert.SerializeObject(notes);
+            File.WriteAllText(filepath, jsonWrite);
+        }
+
+    }
+}
