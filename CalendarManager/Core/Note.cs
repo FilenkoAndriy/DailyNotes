2965fafd3070865f7707b47d8a5ba814ea865c82
ValeriiaAleksieienko <lera.alal@gmail.com>
2019-11-01 21:34:30 +0200

    Added project

---
+++ CalendarManager/Core/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarManager.Core
+{
+    [Serializable]
+    class Note 
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        
+    }
+}
